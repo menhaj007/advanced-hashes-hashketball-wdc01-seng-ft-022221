6d74ba214762b4ff7e07314fc6af6b17b60cdcbe
witty-trace-1906 <menhajuddin.sharaf@gmail.com>
2021-01-10 13:37:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -171,3 +171,16 @@ def num_points_scored(playerName)
         value[:team_name]
     end
 end
+
+def player_numbers(playerName)
+    info = game_hash
+    number = 0
+     list_of_players = info.map do |key, value|
+        value[:players].map do |element|
+            if (element[:player_name] == playerName)
+                number = element[:number]
+            end
+        end
+     end
+     number
+  end
